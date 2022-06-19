bfe221c29c4b915c07d6b265c139dcc4c89eb22f
João Victor <jvictor.franci@gmail.com>
2022-06-19 03:13:52 -0300

    feat: increments login  endpoint response with user data

--- scr/controllers/authController.js
+++ scr/controllers/authController.js
@@ -1,5 +1,5 @@
 import chalk from "chalk";
-import { newUser, searchUser, insertToken } from "../repositories/auth\Repository.js";
+import { newUser, searchUser, insertToken } from "../repositories/authRepository.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
@@ -7,7 +7,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export async function signUp(req, res) {
-    const { email, password, confirmPassword, username, urlpicture} = req.body;
+    const { email, password, username, urlpicture} = req.body;
     const hashPassword= bcrypt.hashSync(password,10);
     try{
        await newUser(username, email, hashPassword, urlpicture);
@@ -24,17 +24,18 @@ export async function signUp(req, res) {
 
 export async function login (req,res){
     try{
-         const {email,password}=req.body;
+         const {email,password} = req.body;
  
-         const verifyUser= await searchUser(email);
-         console.log(verifyUser.rows)
+         const verifyUser = await searchUser(email);
+         console.table(verifyUser.rows)
  
          if(verifyUser.rows.length === 0){
-             res.status(401).send("Usuário Inexistente")
+             res.status(401).send({message: "User not found..."});
+             return;
          }
  
          if(!bcrypt.compareSync(password, verifyUser.rows[0].password)){
-             res.status(401).send("email ou senha incorretos...");
+             res.status(401).send({message : "Wrong password..." });
              return;
          }
  
@@ -46,11 +47,19 @@ export async function login (req,res){
          const token= jwt.sign(data ,process.env.ENCRYPTPASSWORD, config);
  
          await insertToken(verifyUser.rows[0].id,token);
- 
-         res.status(200).send({ token });
+
+         const user = {
+            name : verifyUser.rows[0].username,
+            email : verifyUser.rows[0].email,
+            pictureUrl : verifyUser.rows[0].pictureUrl,
+         }
+
+         const response = { token: token, user: user };
+    
+         res.status(200).send({ message: "Login successful", ...response });
      }
      catch(e){
-         console.log(e)
-         res.sendStatus(400);
+         console.log(e);
+         res.sendStatus(500);
      }
  }
