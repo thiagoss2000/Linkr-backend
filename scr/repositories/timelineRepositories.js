08787169da60356c69cea628787f2fd79085450b
thiagoss2000 <thiago.ss675@gmail.com>
2022-06-23 16:57:14 -0300

    parse_hashtags

--- scr/repositories/timelineRepositories.js
+++ scr/repositories/timelineRepositories.js
@@ -9,24 +9,26 @@ export async function getPosts(req, res) {
     const userId = res.locals.user.id;   
     
     try {
-        const metadata = await connection.query(`SELECT posts.created_at,
-            posts.id, posts.user_id as creat_user, users.id as post_user, users.user_name as "post_userName", 
-            users.image as user_image, posts.link, posts.title, metadata.subject, metadata.presentation, metadata.image
+        const metadata = await connection.query(`SELECT posts.created_at, posts.id, 
+            posts.user_id as creat_user, users.user_name as "creat_userName", users.id as post_user, users.user_name as "post_userName", 
+            users.image as user_image, posts.link, posts.title, metadata.subject, metadata.presentation, metadata.image, likes.id as like
             FROM followers 
             JOIN posts ON followers.followers_id = posts.user_id
             JOIN metadata ON posts.id = metadata.id
             JOIN users ON posts.user_id = users.id
+            LEFT JOIN likes ON likes.user_id = $1 AND likes.post_id = posts.id
             WHERE followers.following_id = $1 AND posts.deleted_at IS NULL
 
-            UNION SELECT re_posts.created_at,
-            posts.id, posts.user_id as creat_user, post_user.id as post_user, post_user.user_name as "post_userName",
-            post_user.image as user_image, posts.link, posts.title, metadata.subject, metadata.presentation, metadata.image
+            UNION SELECT re_posts.created_at, posts.id, 
+            posts.user_id as creat_user, creat_user.user_name as "creat_userName", post_user.id as post_user, post_user.user_name as "post_userName",
+            post_user.image as user_image, posts.link, posts.title, metadata.subject, metadata.presentation, metadata.image, likes.id as like
             FROM followers 
             JOIN re_posts ON followers.followers_id = re_posts.user_id
             LEFT JOIN users post_user ON re_posts.user_id = post_user.id
             LEFT JOIN posts ON re_posts.posts_id = posts.id
             LEFT JOIN metadata ON re_posts.posts_id = metadata.id
             LEFT JOIN users creat_user ON posts.user_id = creat_user.id
+            LEFT JOIN likes ON likes.user_id = $1 AND likes.post_id = posts.id
             WHERE followers.following_id = $1 AND posts.deleted_at IS NULL AND re_posts.deleted_at IS NULL
 
             ORDER BY created_at DESC
@@ -45,32 +47,35 @@ export async function getPosts(req, res) {
 }
 
 export async function getPostsId(req, res) {
-    const { userId } = req.params;
+    const { userId: author_id } = req.params;
     const { page } = req.query;
+    const userId = res.locals.user.id; 
 
     try {
-        const metadata = await connection.query(`SELECT posts.created_at,
-            posts.id, posts.user_id as creat_user, users.id as post_user, users.user_name as "post_userName", 
-            users.image as user_image, posts.link, posts.title, metadata.subject, metadata.presentation, metadata.image
+        const metadata = await connection.query(`SELECT posts.created_at, posts.id, 
+            posts.user_id as creat_user, users.user_name as "creat_userName", users.id as post_user, users.user_name as "post_userName", 
+            users.image as user_image, posts.link, posts.title, metadata.subject, metadata.presentation, metadata.image, likes.id as like
             FROM posts 
             JOIN metadata ON posts.id = metadata.id
             JOIN users ON posts.user_id = users.id
+            LEFT JOIN likes ON likes.user_id = $2 AND likes.post_id = posts.id
             WHERE posts.user_id = $1 AND posts.deleted_at IS NULL
 
-            UNION SELECT re_posts.created_at,
-            posts.id, posts.user_id as creat_user, post_user.id as post_user, post_user.user_name as "post_userName",
-            post_user.image as user_image, posts.link, posts.title, metadata.subject, metadata.presentation, metadata.image
+            UNION SELECT re_posts.created_at, posts.id, 
+            posts.user_id as creat_user, creat_user.user_name as "creat_userName", post_user.id as post_user, post_user.user_name as "post_userName",
+            post_user.image as user_image, posts.link, posts.title, metadata.subject, metadata.presentation, metadata.image, likes.id as like
             FROM re_posts 
             JOIN posts ON re_posts.posts_id = posts.id
             LEFT JOIN users post_user ON re_posts.user_id = post_user.id
             LEFT JOIN metadata ON re_posts.posts_id = metadata.id
             LEFT JOIN users creat_user ON posts.user_id = creat_user.id
+            LEFT JOIN likes ON likes.user_id = $2 AND likes.post_id = posts.id
             WHERE re_posts.user_id = $1 AND posts.deleted_at IS NULL AND re_posts.deleted_at IS NULL
 
             ORDER BY created_at DESC
             LIMIT ${limitSearch}
             OFFSET ${page * limitSearch}
-        `, [userId]);
+        `, [author_id, userId]);
 
         const object = {
             posts: metadata.rows,
@@ -98,7 +103,7 @@ export async function postTimeline(req, res, next) {
        
         res.local = {
             post_id: id.rows[0].id,
-            hashtags: wordKeys.map(e => e.split(' ')[0])
+            hashtags: wordKeys
         }
         next();
         res.sendStatus(201);
@@ -110,13 +115,18 @@ export async function postTimeline(req, res, next) {
 export async function putTimeline(req, res) {
     const id = req.params;
     const { title } = req.body;
+    const wordKeys = findHashtags(title);
     try {
         const putId = await connection.query(`UPDATE posts SET title = $1
             WHERE id = $2 AND user_id = $3 RETURNING id
         `, [title, id.postId, res.locals.user.id]);
 
         if(putId.rows.length == 0) return res.sendStatus(404);
-
+        res.local = {
+            post_id: putId.rows[0].id,
+            hashtags: wordKeys
+        }
+        next();
         res.sendStatus(200);
     } catch (e){
         res.sendStatus(500);
@@ -146,10 +156,10 @@ export async function rePostTimeline(req, res) {
         console.log(validId)
         if(validId.rows.length == 0) return res.sendStatus(404);
 
-        const insertId = await connection.query(`INSERT INTO re_posts (posts_id, user_id)
+        const inserId = await connection.query(`INSERT INTO re_posts (posts_id, user_id)
             VALUES ($1, $2) RETURNING id
         `, [post_id, res.locals.user.id]);
-        if(insertId.rows.length == 0) return res.sendStatus(404);
+        if(inserId.rows.length == 0) return res.sendStatus(404);
 
         res.sendStatus(201);
     } catch (e){
@@ -170,3 +180,4 @@ export async function deleteRePost(req, res) {
         res.sendStatus(500);
     }
 }
+
